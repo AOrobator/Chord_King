6f8def57b5fefffdaf48f58a063d1996e03d809a
Andrew Orobator <orobator.andrew@gmail.com>
2021-12-22 23:42:52 -0500

    worked on main card ui

--- app/src/main/java/com/orobator/chordking/MainActivity.kt
+++ app/src/main/java/com/orobator/chordking/MainActivity.kt
@@ -3,10 +3,19 @@ package com.orobator.chordking
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.orobator.chordking.ui.theme.ChordKingTheme
+import com.orobator.chordking.ui.theme.Colors
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,15 +34,52 @@ fun ChordKingHomeScreen() {
         Scaffold(
             topBar = { ChordKingAppBar() }
         ) {
+            Column(
+                modifier = Modifier.padding(
+                    vertical = 32.dp,
+                    horizontal = 24.dp
+                ),
+                verticalArrangement = spacedBy(32.dp)
+            ) {
+                HomeSelectionCard(
+                    text = "Build a chord",
+                    backgroundColor = Colors.Purple300
+                )
 
+                HomeSelectionCard(
+                    text = "Name that chord",
+                    backgroundColor = Colors.Blue300
+                )
+            }
         }
+    }
+}
+
+@Composable
+private fun ColumnScope.HomeSelectionCard(
+    text: String,
+    backgroundColor: Color
+) {
+    Card(
+        modifier = Modifier
+            .weight(1f)
+            .fillMaxWidth(),
+        backgroundColor = backgroundColor
+    ) {
+        Text(text)
     }
 }
 
 @Composable
 private fun ChordKingAppBar() {
     TopAppBar(
-        title = { Text(text = "👑 Chord King") }
+        title = {
+            Text(
+                text = "👑 Chord King",
+                color = Color.White
+            )
+        },
+        backgroundColor = Colors.Purple500
     )
 }
 
--- app/src/main/java/com/orobator/chordking/ui/theme/Color.kt
+++ app/src/main/java/com/orobator/chordking/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package com.orobator.chordking.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+object Colors {
+    val Blue300 = Color(0xFF64B5F6)
+    val Purple200 = Color(0xFFBB86FC)
+    val Purple300 = Color(0xFFBA68C8)
+    val Purple500 = Color(0xFF6200EE)
+    val Purple700 = Color(0xFF3700B3)
+    val Teal200 = Color(0xFF03DAC5)
+}
