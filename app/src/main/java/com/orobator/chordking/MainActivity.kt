d0919921bcb23e345ebc55bfd2d9e85588120b1b
Andrew Orobator <orobator.andrew@gmail.com>
2021-12-22 23:48:18 -0500

    Polished cards in light mode

--- app/src/main/java/com/orobator/chordking/MainActivity.kt
+++ app/src/main/java/com/orobator/chordking/MainActivity.kt
@@ -3,17 +3,17 @@ package com.orobator.chordking
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.orobator.chordking.ui.theme.ChordKingTheme
 import com.orobator.chordking.ui.theme.Colors
 
@@ -64,9 +64,20 @@ private fun ColumnScope.HomeSelectionCard(
         modifier = Modifier
             .weight(1f)
             .fillMaxWidth(),
-        backgroundColor = backgroundColor
+        backgroundColor = backgroundColor,
+        elevation = 8.dp,
+        shape = RoundedCornerShape(24.dp)
     ) {
-        Text(text)
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = text,
+                color = Color.White,
+                fontSize = 32.sp
+            )
+        }
     }
 }
 
